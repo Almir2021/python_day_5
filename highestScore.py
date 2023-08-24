1f6a77e9bb15e4612c7b59205598e33391fff5ad
Almir2021 <bosnjicalmir@gmail.com>
2023-08-24 21:50:11 +0200

    frist try at algoritm, getting there

---
+++ highestScore.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+print(student_scores)
+
+lenghtOflist = 0
+
+for i in student_scores:
+  currentMax =0
+  
+  if student_scores[currentMax] < student_scores[i] :
+    hightest  =   student_scores[i]
+currentMax = +1
+
+
+print(hightest)
