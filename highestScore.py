d5afa5b41a019a1a3a5e549b6af40990431c1610
Almir2021 <bosnjicalmir@gmail.com>
2023-08-26 22:59:11 +0200

    finally complete , but still not satisfied . need more practise

--- highestScore.py
+++ highestScore.py
@@ -6,22 +6,21 @@ print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-sequence = 0
-print(student_scores)
+sequence = student_scores
+
 
 lenghtOflist = 0
 
 for x in student_scores :
   lenghtOflist += 1
 
-print(lenghtOflist)  
+ 
 
-def maxFuction(*args):
    
-  if lenghtOflist ==1:
-     sequence = args[0]
-  else :
-    sequence = args
+if lenghtOflist ==1:
+     sequence = student_scores[0]
+else :
+    sequence =  student_scores
  
    
 largest = sequence[0]  
@@ -30,8 +29,8 @@ for item in sequence:
  if  item > largest:
    largest = item
 
-return largest
+print(f"The highest score in the class is:{largest}")
 
-number = 
+
   
   
